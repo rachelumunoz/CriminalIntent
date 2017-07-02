d5b0d35c0aa5c260c0ea9ca7d4baf6d27c225416
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-02 15:08:03 -0700

    Adds abstract class for activities that host a single fragment, refactors CrimeActivity to extend SingleFragmentActivity

---
+++ app/src/main/java/io/rachelmunoz/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package io.rachelmunoz.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rachelmunoz on 7/2/17.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+	protected abstract Fragment createFragment();
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fragment); // container for whole view
+
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragment_container); // in Activity xml, specific spot fragment will go
+
+		if (fragment == null){
+			fragment = createFragment();
+			fm.beginTransaction() // FragmentTransaction returned so can compose
+					.add(R.id.fragment_container, fragment) // add the fragment to its spot
+					.commit();
+		}
+
+
+	}
+}
