798eed93653093a64396b99ce3b32bda63124e06
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-02 16:52:28 -0700

    Wires up RecyclerView widget

--- app/src/main/java/io/rachelmunoz/criminalintent/CrimeListFragment.java
+++ app/src/main/java/io/rachelmunoz/criminalintent/CrimeListFragment.java
@@ -1,10 +1,30 @@
 package io.rachelmunoz.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by rachelmunoz on 7/2/17.
  */
 
 public class CrimeListFragment extends Fragment {
+	private RecyclerView mCrimeRecyclerView;
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_crime_list, container, false); // inflate the fragment XML
+
+		mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view); // find the RecyclerView widget
+		mCrimeRecyclerView.setLayoutManager( new LinearLayoutManager(getActivity())); // need to set LayoutManager to position items on sceen
+
+		return view;
+
+	}
 }
