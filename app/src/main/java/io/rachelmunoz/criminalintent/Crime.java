c528c86ef52f62dbac2779c4df5badac1f3c2505
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-01 20:19:42 -0700

    Creates Crime model layer

---
+++ app/src/main/java/io/rachelmunoz/criminalintent/Crime.java
@@ -0,0 +1,44 @@
+package io.rachelmunoz.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by rachelmunoz on 7/1/17.
+ */
+
+public class Crime {
+	private UUID mId;
+	private String mTitle;
+	private Date mDate;
+	private boolean mSolved;
+
+	public Crime(){
+		mDate = new Date();
+		mId = UUID.randomUUID();
+	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public String getTitle() {
+		return mTitle;
+	}
+
+	public void setTitle(String title) {
+		mTitle = title;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean solved) {
+		mSolved = solved;
+	}
+}
