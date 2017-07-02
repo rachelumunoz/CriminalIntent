c6484fcff87ababda85f9e99a68c1f6a4c6b4cbe
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-01 21:23:58 -0700

    Inflates fragment and wires up widgets

---
+++ app/src/main/java/io/rachelmunoz/criminalintent/CrimeFragment.java
@@ -0,0 +1,71 @@
+package io.rachelmunoz.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import static android.widget.CompoundButton.*;
+
+/**
+ * Created by rachelmunoz on 7/1/17.
+ */
+
+public class CrimeFragment extends Fragment {
+	private Crime mCrime;
+	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckbox;
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mCrime = new Crime();
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+		mTitleField = (EditText) v.findViewById(R.id.crime_title);
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+				mCrime.setTitle(charSequence.toString());
+			}
+
+			@Override
+			public void afterTextChanged(Editable editable) {
+
+			}
+		});
+
+		mDateButton = (Button) v.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+
+		mSolvedCheckbox = (CheckBox) v.findViewById(R.id.crime_solved);
+		mSolvedCheckbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+				mCrime.setSolved(b);
+			}
+		});
+
+		return v;
+	}
+}
