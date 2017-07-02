09a4ac7d970314ce1b03c6ebf44e7ce0026b77e7
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-02 15:33:46 -0700

    Adds Activity and Fragment to display crimes, updates manifest to launch CrimeListActivity on launch

---
+++ app/src/main/java/io/rachelmunoz/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package io.rachelmunoz.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by rachelmunoz on 7/2/17.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
