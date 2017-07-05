7f83c1f9c72cfd49e699603c044ac951b78f664b
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-04 22:08:24 -0700

    Adds CrimePagerActivity and XML, adds FragmentStatePageAdapter

---
+++ app/src/main/java/io/rachelmunoz/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,45 @@
+package io.rachelmunoz.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+/**
+ * Created by rachelmunoz on 7/4/17.
+ */
+
+public class CrimePagerActivity extends AppCompatActivity {
+	private ViewPager mViewPager;
+	private List<Crime> mCrimes;
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime_pager);
+
+		mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+
+		mCrimes = CrimeLab.get(this).getCrimes();
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+			@Override
+			public Fragment getItem(int position) {
+				Crime crime = mCrimes.get(position);
+				return CrimeFragment.newInstance(crime.getId());
+			}
+
+			@Override
+			public int getCount() {
+				mCrimes.size();
+			}
+		});
+
+	}
+}
