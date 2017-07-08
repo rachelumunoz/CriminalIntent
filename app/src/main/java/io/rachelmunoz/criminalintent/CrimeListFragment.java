1f9be36ccb1e94cc621af419d66817877a5bfc4f
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-08 14:49:43 -0700

    Adds toggle of subtitle by setting a flag of whether or not the id of the show subtitle item is found

--- app/src/main/java/io/rachelmunoz/criminalintent/CrimeListFragment.java
+++ app/src/main/java/io/rachelmunoz/criminalintent/CrimeListFragment.java
@@ -5,8 +5,10 @@ import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -28,6 +30,7 @@ import java.util.List;
 public class CrimeListFragment extends Fragment {
 	private RecyclerView mCrimeRecyclerView;
 	private CrimeAdapter mAdapter;
+	private boolean mSubtitleVisible;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,6 +48,14 @@ public class CrimeListFragment extends Fragment {
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.fragment_crime_list, menu);
+
+		MenuItem subtitleItem = menu.findItem(R.id.show_subtitle);
+		if (mSubtitleVisible){
+			subtitleItem.setTitle(R.string.hide_subtitle);
+		} else {
+			subtitleItem.setTitle(R.string.show_subtitle);
+		}
+
 	}
 
 	@Override
@@ -56,9 +67,27 @@ public class CrimeListFragment extends Fragment {
 				Intent intent = CrimePagerActivity.newIntent(getActivity(), crime.getId());
 				startActivity(intent);
 				return true;
+			case R.id.show_subtitle:
+				mSubtitleVisible = !mSubtitleVisible;
+				getActivity().invalidateOptionsMenu();
+				updateSubtitle();
+				return true;
 			default:
 				return super.onOptionsItemSelected(item);
 		}
+	}
+
+	private void updateSubtitle(){
+		CrimeLab crimeLab = CrimeLab.get(getActivity());
+		int crimeCount = crimeLab.getCrimes().size();
+		String subtitle = getString(R.string.subtitle_format, crimeCount);
+
+		if (!mSubtitleVisible){
+			subtitle = null;
+		}
+
+		AppCompatActivity activity = (AppCompatActivity) getActivity();
+		activity.getSupportActionBar().setSubtitle(subtitle);
 	}
 
 	@Nullable
@@ -143,5 +172,7 @@ public class CrimeListFragment extends Fragment {
 		} else {
 			mAdapter.notifyDataSetChanged();
 		}
+
+		updateSubtitle();
 	}
 }
