f298d3042215e572b227a057c4276f0ebbb5ef3a
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-14 15:32:14 -0700

    Adds suspect field to Crime, adds suspect column code for read, write. Add strings for upcoming functionality

--- app/src/main/java/io/rachelmunoz/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/io/rachelmunoz/criminalintent/database/CrimeBaseHelper.java
@@ -25,7 +25,8 @@ public class CrimeBaseHelper extends SQLiteOpenHelper { // creates DB and config
 			CrimeTable.Cols.UUID + ", " +
 			CrimeTable.Cols.TITLE + ", " +
 			CrimeTable.Cols.DATE + ", " +
-			CrimeTable.Cols.SOLVED +
+			CrimeTable.Cols.SOLVED + ", " +
+			CrimeTable.Cols.SUSPECT +
 			")"
 		);
 	}
