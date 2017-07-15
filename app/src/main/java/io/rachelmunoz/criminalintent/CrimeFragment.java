29aa22632a819fb3b93c228478f622a49feb5220
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-14 21:14:10 -0700

    Sends intent to take photo on click of photobutton

--- app/src/main/java/io/rachelmunoz/criminalintent/CrimeFragment.java
+++ app/src/main/java/io/rachelmunoz/criminalintent/CrimeFragment.java
@@ -3,13 +3,16 @@ package io.rachelmunoz.criminalintent;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.FileProvider;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -23,7 +26,9 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.File;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import static android.widget.CompoundButton.*;
@@ -34,6 +39,7 @@ import static android.widget.CompoundButton.*;
 
 public class CrimeFragment extends Fragment {
 	private Crime mCrime;
+	private File mPhotoFile;
 	private EditText mTitleField;
 	private Button mDateButton;
 	private CheckBox mCrimeSolved;
@@ -44,8 +50,10 @@ public class CrimeFragment extends Fragment {
 
 	private static final String ARG_CRIME_ID = "crime_id";
 	private static final String DIALOG_DATE = "DialogDate";
+
 	private static final int REQUEST_DATE = 0;
 	private static final int REQUEST_CONTACT = 1;
+	private static final int REQUEST_PHOTO = 2;
 
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,7 +61,7 @@ public class CrimeFragment extends Fragment {
 
 		UUID crimeID = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
 		mCrime = CrimeLab.get(getActivity()).getCrime(crimeID);
-
+		mPhotoFile = CrimeLab.get(getActivity()).getPhotoFile(mCrime);
 	}
 
 	@Override
@@ -148,6 +156,41 @@ public class CrimeFragment extends Fragment {
 
 		mPhotoView = (ImageView) v.findViewById(R.id.crime_photo);
 		mPhotoButton = (ImageButton) v.findViewById(R.id.crime_camera);
+
+
+		final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // implicit intent action
+
+		boolean canTakePhoto = mPhotoFile != null && captureImage.resolveActivity(packageManager) != null; // place to save && activity to use
+		mPhotoButton.setEnabled(canTakePhoto);
+
+		mPhotoButton.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View view) {
+				Uri uri = FileProvider.getUriForFile(
+						getActivity(),
+						"io.rachelmunoz.criminalintent.fileprovider", // URI pointing to where to save large res photo
+						mPhotoFile
+					);
+				captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+
+				List<ResolveInfo> cameraActivities = getActivity()
+														.getPackageManager()
+														.queryIntentActivities(  // find all possible activities
+																captureImage,
+																PackageManager.MATCH_DEFAULT_ONLY);
+
+				for (ResolveInfo activity : cameraActivities){
+					getActivity()
+						.grantUriPermission(
+							activity.activityInfo.packageName,       // give them permission
+							uri,
+							Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+						);
+				}
+
+				startActivityForResult(captureImage, REQUEST_PHOTO);
+			}
+		});
 
 		return v;
 	}
