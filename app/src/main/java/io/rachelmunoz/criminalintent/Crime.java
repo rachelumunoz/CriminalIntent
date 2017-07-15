92f022548f558064ad6c7f95453059e690bbeddb
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-14 20:03:51 -0700

    Adds methods to model layer to retrieve File objects

--- app/src/main/java/io/rachelmunoz/criminalintent/Crime.java
+++ app/src/main/java/io/rachelmunoz/criminalintent/Crime.java
@@ -58,4 +58,10 @@ public class Crime {
 	public void setSuspect(String suspect) {
 		mSuspect = suspect;
 	}
+
+	public String getPhotoFilename(){
+		return "IMG_" + getId().toString() + ".jpg";
+	}
 }
+
+
