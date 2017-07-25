9395a035fa1c471bad39317116bd10bc4b2e3476
Rachel Munoz <rachelumunoz@gmail.com>
2017-07-24 21:07:45 -0700

    Adds alias resources for tablet, adds master-detail interface for large screens

--- app/src/main/java/io/rachelmunoz/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/io/rachelmunoz/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package io.rachelmunoz.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,10 +14,15 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 	protected abstract Fragment createFragment();
 
+	@LayoutRes
+	protected int getLayoutResId(){
+		return R.layout.activity_fragment;
+	}
+
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_fragment); // container for whole view
+		setContentView(getLayoutResId()); // container for whole view
 
 		FragmentManager fm = getSupportFragmentManager();
 		Fragment fragment = fm.findFragmentById(R.id.fragment_container); // in Activity xml, specific spot fragment will go
